eeba1be5c290cc0f26931d11b437f44e4a3bdec8
Eiman Butt <163169388+EimanButt07@users.noreply.github.com>
2024-05-09 00:37:52 +0500

    BMI Calculator
    It`s an CLI based BMI Calculator.

---
+++ calculator.js
@@ -0,0 +1,5 @@
+var weightInKg = "20";
+var heightInMeters = "2";
+// let bmi = weightInKg / (heightInMeters * heightInMeters)
+var bmi = 20 / 2;
+console.log("your bmi is ".concat(bmi));
---
+++ calculator.ts
@@ -0,0 +1,5 @@
+let weightInKg= "20"
+let heightInMeters = "2"
+// let bmi = weightInKg / (heightInMeters * heightInMeters)
+let bmi = 20 / 2
+console.log(`your bmi is ${bmi}`)
